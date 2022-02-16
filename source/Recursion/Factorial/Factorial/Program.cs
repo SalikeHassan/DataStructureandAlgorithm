7fc1c3528ddc39e13bbc0a1dccf219e42b807bbf
Salike Hassan <mfsi.salike@gmail.com>
2022-02-17 00:42:00 +0530

    Factorial and Power using recursion

---
+++ source/Recursion/Factorial/Factorial/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = int.MinValue;
+            Console.WriteLine("Enter number");
+            if (int.TryParse(Console.ReadLine(), out input))
+            {
+                Console.WriteLine($"Factorial of {input} = {CaluclateFact(input)}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+        }
+
+        static int CaluclateFact(int num)
+        {
+            if (num == 0)
+            {
+                return 1;
+            }
+
+            else
+            {
+                return num * CaluclateFact(num - 1);
+            }
+        }
+    }
+}
---
+++ source/Recursion/Power/Power/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Power
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var num = double.MinValue;
+            var pow = int.MinValue;
+            Console.WriteLine("Input number");
+            num = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Input power");
+            pow = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{CalculatePow(num, pow)}");
+        }
+
+        static double CalculatePow(double num, int pow)
+        {
+
+            if (pow < 0)
+            {
+                num = 1 / num;
+                pow = -pow;
+            }
+
+            return GetPow(num, pow);
+        }
+
+        private static double GetPow(double num, int pow)
+        {
+            if (pow == 0)
+            {
+                return 1;
+            }
+
+            else
+            {
+                return num * GetPow(num, pow - 1);
+            }
+        }
+    }
+}
